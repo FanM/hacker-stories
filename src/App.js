04df6da101390b7c7d65b9519b80a8ace5129a9c
Fan <maofan1983@gmail.com>
2021-05-09 14:01:06 +0000

    Added useEffect for storing searchTerm to localStorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   const welcome = {
     greeting: "Hello",
-    title: "Fan"
+    title: "Fan",
   };
 
   const stories = [
@@ -15,7 +15,7 @@ function App() {
       author: "Jordan Walke",
       num_comments: 3,
       points: 4,
-      objectID: 0
+      objectID: 0,
     },
     {
       title: "Redux",
@@ -23,53 +23,57 @@ function App() {
       author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
-      objectID: 1
-    }
+      objectID: 1,
+    },
   ];
 
-  const handleSearch = e => console.log(e.target.value);
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem("search") || "React"
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLocaleLowerCase().includes(searchTerm)
+  );
 
   return (
     <div className="App">
       <h1>
         {welcome.greeting}! {welcome.title}
       </h1>
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
       <hr />
-      <List list={stories} />
-    </div>
-  );
-};
-
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      {" "}
-      <span>
-        <a href={item.url}>{item.title}</a>{" "}
-      </span>
-      <span>{item.author}</span> <span>{item.num_comments}</span>{" "}
-      <span>{item.points}</span>
-    </div>
-  ));
-
-const Search = props => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  const handleChange = e => {
-    setSearchTerm(e.target.value);
-    props.onSearch(e);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <List list={searchedStories} />
     </div>
   );
 }
+
+const List = (props) =>
+  props.list.map(({objectID, ...item}) => <Item key={objectID} {...item} />);
+
+const Item = ({title, url, author, num_comments, points}) => (
+  <div>
+    <span>
+      <a href={url}>{title}</a>{" "}
+    </span>
+    <span>{author}</span> <span>{num_comments}</span> <span>{points}</span>
+  </div>
+);
+
+const Search = ({search, onSearch}) => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={search} onChange={onSearch} />
+    </div>
+  );
+};
 
 export default App;
